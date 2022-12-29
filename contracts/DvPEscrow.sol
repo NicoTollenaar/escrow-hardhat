c0d716ac9ff860fcd5bef04638b1860875707b7f
NicoTollenaar <nico.tollenaar@gmail.com>
2022-12-29 09:25:10 +0100

    further amends to escrow contract, done nothing more on tests

--- contracts/DvPEscrow.sol
+++ contracts/DvPEscrow.sol
@@ -17,6 +17,9 @@ contract DvPEscrow {
     bool public isSaleObjectTransferred;
     bool public hasTransactionClosed;
 
+    bool public isPurchasePriceWithdrawn;
+    bool public isSaleObjectWithdrawn;
+
     constructor(
         address _seller,
         address _buyer,
@@ -27,10 +30,6 @@ contract DvPEscrow {
         uint _purchasePrice,
         uint _transactionDeadline
     ) payable {
-        require(
-            msg.sender == seller || msg.sender == buyer,
-            "deployer must be seller or buyer"
-        );
         require(
             msg.value >= 0.1 ether,
             "insufficient ETH sent to fund the escrow contract"
@@ -44,58 +43,15 @@ contract DvPEscrow {
         currencyContractAddress = _currencyContractAddress;
         purchasePrice = _purchasePrice;
         transactionDeadline = _transactionDeadline;
-        if (msg.sender == seller) {
-            transferSaleObjectToEscrow();
-        } else if (msg.sender == buyer) {
-            transferPurchasePriceToEscrow();
-        }
     }
 
-    function depositSaleObjectInEscrow() external {
+    function transferSaleObjectIntoEscrow() external {
         require(msg.sender == seller, "only seller can transfer asset");
         require(
             block.timestamp < transactionDeadline,
             "transaction deadline expire"
         );
-        require(
-            isPurchasePricePaid,
-            "purchase price not yet paid into escrow, something went wrong at deployment"
-        );
-        bool success = transferSaleObjectToEscrow();
-        require(success, "transferring sale object to escrow failed");
-        closeTransaction();
-    }
-
-    function depositPurchasePriceInEscrow() external {
-        require(msg.sender == buyer, "only buyer can transfer asset");
-        require(
-            block.timestamp < transactionDeadline,
-            "transaction deadline expire"
-        );
-        require(
-            isSaleObjectTransferred,
-            "sale object not transferred, something went wrong at deployment"
-        );
-        bool success = transferPurchasePriceToEscrow();
-        require(success, "transferring purchase price to escrow failed");
-        closeTransaction();
-    }
-
-    function transferPurchasePriceToEscrow() private returns (bool) {
-        (bool success, ) = currencyContractAddress.call(
-            abi.encodeWithSignature(
-                "transferFrom(address,address,uint256)",
-                msg.sender,
-                address(this),
-                purchasePrice
-            )
-        );
-        require(success, "transferring purchase price to escrow failed");
-        isPurchasePricePaid = true;
-        return success;
-    }
-
-    function transferSaleObjectToEscrow() private returns (bool) {
+        require(!isSaleObjectTransferred, "sale object already transferred");
         (bool success, ) = saleObjectContractAddress.call(
             abi.encodeWithSignature(
                 "transferFrom(address,address,uint256)",
@@ -106,19 +62,30 @@ contract DvPEscrow {
         );
         require(success, "transferring sale object to escrow failed");
         isSaleObjectTransferred = true;
-        return success;
+        if (isPurchasePricePaid) closeTransaction();
+    }
+
+    function transferPurchasePriceIntoEscrow() external {
+        require(msg.sender == buyer, "only buyer can transfer asset");
+        require(
+            block.timestamp < transactionDeadline,
+            "transaction deadline expire"
+        );
+        require(!isPurchasePricePaid, "purchase price already paid");
+
+        if (isSaleObjectTransferred) closeTransaction();
     }
 
     function closeTransaction() private {
         require(
             isPurchasePricePaid && isSaleObjectTransferred,
-            "both transaction legs not yet performed"
+            "transaction legs not yet both performed"
         );
         bool cashLegSettled = transferPurchasePriceToSeller();
         bool assetLegSettled = transferSaleObjectToBuyer();
         require(cashLegSettled && assetLegSettled);
         hasTransactionClosed = true;
-        transferAccruedInterestToBuyer();
+        transferAccruedInterestToDeployer();
         transferResidualETHToDeployer();
     }
 
@@ -148,6 +115,10 @@ contract DvPEscrow {
 
     function withdrawPurchasePrice() external {
         require(
+            isPurchasePricePaid,
+            "purchase price not paid, nothing to withdraw"
+        );
+        require(
             block.timestamp > transactionDeadline,
             "transaction deadline not yet expired"
         );
@@ -164,11 +135,17 @@ contract DvPEscrow {
             )
         );
         require(success, "withdrawal of purchase price failed");
-        transferAccruedInterestToBuyer();
-        transferResidualETHToDeployer();
+        isPurchasePriceWithdrawn = true;
+        transferAccruedInterestToDeployer();
+        if (!isSaleObjectTransferred || isSaleObjectWithdrawn)
+            transferResidualETHToDeployer();
     }
 
     function withdrawSaleObject() external {
+        require(
+            isSaleObjectTransferred,
+            "sale object not transferred into escrow, nothing to withdraw"
+        );
         require(
             block.timestamp > transactionDeadline,
             "transaction deadline not yet expired"
@@ -186,28 +163,56 @@ contract DvPEscrow {
             )
         );
         require(success, "withdrawal of purchase price failed");
-        transferResidualETHToDeployer();
+        if (!isPurchasePricePaid || isPurchasePriceWithdrawn)
+            transferResidualETHToDeployer();
     }
 
-    function transferAccruedInterestToBuyer() private {
-        (bool success1, bytes memory accruedInterest) = currencyContractAddress
-            .call(abi.encodeWithSignature("balanceOf(address)", address(this)));
-        require(success1, "getting balance accrued interest failed");
-        (bool success2, ) = currencyContractAddress.call(
+    function transferAccruedInterestToDeployer() private {
+        uint accruedInterest = getBalanceOfCurrency();
+        (bool success, ) = currencyContractAddress.call(
             abi.encodeWithSignature(
                 "transfer(address,uint256)",
-                buyer,
+                deployer,
                 accruedInterest
             )
         );
-        require(success2, "transfer of accrued interest to buyer failed");
+        require(success, "transfer of accrued interest to deployer failed");
     }
 
-    function transferResidualETHToDeployer() private {
+    function transferResidualETHToDeployer() public {
+        bool isEscrowExpiredAndEmpty = checkEscrowExpiredAndEmpty();
+        require(
+            hasTransactionClosed || isEscrowExpiredAndEmpty,
+            "escrow not yet completed or expired and empty"
+        );
         (bool success, ) = payable(deployer).call{value: address(this).balance}(
             ""
         );
         require(success, "transfer of remaining ether to deployer failed");
+    }
+
+    function checkEscrowExpiredAndEmpty() private returns (bool) {
+        return
+            (block.timestamp > transactionDeadline) &&
+            (getBalanceOfCurrency() + getBalanceOfSaleObject() == 0);
+    }
+
+    function getBalanceOfCurrency() private returns (uint) {
+        (bool success, bytes memory balanceCurrency) = currencyContractAddress
+            .call(abi.encodeWithSignature("balanceOf(address)", address(this)));
+        require(success, "getting balance currency failed");
+        return uint(bytes32(balanceCurrency));
+    }
+
+    function getBalanceOfSaleObject() private returns (uint) {
+        (
+            bool success,
+            bytes memory balanceSaleObject
+        ) = saleObjectContractAddress.call(
+                abi.encodeWithSignature("balanceOf(address)", address(this))
+            );
+        require(success, "getting balance of SaleObject failed");
+        return uint(bytes32(balanceSaleObject));
     }
 
     receive() external payable {}
